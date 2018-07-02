413b59c5814a9b0859b974a8692a0c449cc358aa
pblomena <pblomena@hotmail.com>
2018-07-02 13:20:51 -0400

    actualiza10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,12 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ContactProvider } from '../providers/contact/contact';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,17 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyAf8twZXD-0eIoXPlbJ6ScBcXEM7SDybQQ",
+      authDomain: "epe2-6d92d.firebaseapp.com",
+      databaseURL: "https://epe2-6d92d.firebaseio.com",
+      projectId: "epe2-6d92d",
+      storageBucket: "epe2-6d92d.appspot.com",
+      messagingSenderId: "463303010991"
+
+    }),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,7 +38,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ContactProvider
   ]
 })
 export class AppModule {}
