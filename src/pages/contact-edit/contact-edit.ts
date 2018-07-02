7b1f2d2163c72e06c192788c62237f20664562d6
pblomena <pblomena@hotmail.com>
2018-07-02 14:44:04 -0400

    contacedit

--- src/pages/contact-edit/contact-edit.ts
+++ src/pages/contact-edit/contact-edit.ts
@@ -22,24 +22,13 @@ export class ContactEditPage {
     this.contact = this.navParams.data.contact || { };
     this.createForm();
  
-    // // maneira 2
-    // this.contact = { };
-    // this.createForm();
- 
-    // if (this.navParams.data.key) {
-    //   const subscribe = this.provider.get(this.navParams.data.key).subscribe((c: any) => {
-    //     subscribe.unsubscribe();
- 
-    //     this.contact = c;
-    //     this.createForm();
-    //   })
-    // }
+   
  
     this.setupPageTitle();
   }
  
   private setupPageTitle() {
-    this.title = this.navParams.data.contact ? 'Alterando contato' : 'Novo contato';
+    this.title = this.navParams.data.contact ? 'Contacto Editado' : 'Nuevo Contacto';
   }
  
   createForm() {
@@ -54,11 +43,11 @@ export class ContactEditPage {
     if (this.form.valid) {
       this.provider.save(this.form.value)
         .then(() => {
-          this.toast.create({ message: 'Contato salvo com sucesso.', duration: 3000 }).present();
+          this.toast.create({ message: 'Contacto guardado Exitosamente.', duration: 3000 }).present();
           this.navCtrl.pop();
         })
         .catch((e) => {
-          this.toast.create({ message: 'Erro ao salvar o contato.', duration: 3000 }).present();
+          this.toast.create({ message: 'Error al Guardar Contacto.', duration: 3000 }).present();
           console.error(e);
         })
     }
